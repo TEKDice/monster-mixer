9ad92145eebb86384138db8a0c969279a0205061
Kyle Kemp <imnothere199@gmail.com>
2013-01-19 12:38:47 -0600

    adding index, removing readme

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TEKDice Monster Mixer</title>
+		<?php $pos='../'; include('../include/head.php'); ?>
+	</head>
+	<body>
+		<?php include('../include/body.php'); ?>
+		<div class="container">
+			<div class="push"></div>
+			<div class="pagination-centered hero-unit" id="content">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a nisl tellus. Suspendisse egestas lorem eleifend risus gravida laoreet. Duis pellentesque dolor at sapien euismod at tristique metus scelerisque. Sed porttitor nibh eget felis dictum luctus. Integer lorem quam, placerat in vestibulum id, gravida sed libero. Vestibulum eu risus risus. Ut sodales erat eget nulla egestas consequat. Cras euismod tempus mattis. Maecenas vulputate molestie congue. Maecenas fringilla auctor ultrices.
+			</div>
+		</div>
+		<?php include('../include/foot.php'); ?>
+	</body>
+</html>
