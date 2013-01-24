30406bbf6c57c6a509abe0a827d4498ec3c146da
Kyle <kyle@tekdice.com>
2013-01-24 17:14:25 -0600

    initial version of the attribute selector works properly (no joined functions work yet)

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+
+<?php
+define('readonly',true);
+
+include('../include/database.php');
+
+global $conn;
+	
+$filterNamesArr = run_query_arr($conn, "select name,is_numeric,sql_name,join_table,join_table_col,link_table from GeneratorFilters");
+$filterNames = array();
+
+$json_obj = new stdClass;
+
+foreach($filterNamesArr as $name=>$filter) {
+	if($filter["is_numeric"] == 1)
+		$filterNames[$filter["name"]] = true;
+	else if($filter["name"] == "Name")
+		$filterNames[$filter["name"]] = false;
+	else {
+		$filterNames[$filter["name"]] = array();
+
+		$filterData = run_query_arr($conn, "select $filter[sql_name] from $filter[join_table] where (hidden=0 or hidden is null)");
+		foreach($filterData as $col=>$val) {
+			array_push($filterNames[$filter["name"]], $val[$filter['sql_name']]);
+		}
+	}
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -7,6 +37,9 @@
 
 		<script type="application/javascript" src="js/iscroll.js"></script>
 		<script src="js/init.js"></script>
+		<script type="text/javascript">
+			var filterData = <?=json_encode($filterNames, JSON_PRETTY_PRINT); ?>
+		</script>
 	</head>
 	<body>
 		<?php include('../include/body.php'); ?>
@@ -19,96 +52,37 @@
 			<center><div id="showFilters"><i id="filterIcon" class="icon-arrow-down"></i></div></center>
 			<div class="container-fluid" id="mainContainer">
 				<div class="row-fluid" id="main">
+
 					<div class="span9" id="monstersContainer">
 						<div class="tabbable tabs-left" id="monsterListCont">
 							<ul class="nav nav-tabs" id="monsterList">
 								<li>
 									<a href="#1A" data-toggle="tab">Devourer</a>
 								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
-								<li>
-									<a href="#1A" data-toggle="tab">Devourer</a>
-								</li>
 							</ul>
-							<div class="tab-content">
+							<div class="tab-content" id="monsterData">
 								<div class="tab-pane" id="1A">
 									DevourerContent
 								</div>
-								<div class="tab-pane" id="1B">
-									DevourerContent2
+							</div>
+						</div>
+
+						<div id="encounterStats">
+							Encounter Statistics (CR, Avg. Level, Avg. Dmg. Dealt, Avg. Creature Dmg. Taken, Total Damage Dealt/Taken)
+						</div>
+					</div>
+
+					<div class="span3" id="rightCol">
+						<div id="customRoller">
+							<div class="control-group" id="rollerContainer">
+								<div class="controls">
+									<div class="input-append">
+										<input type="text" class="input-block-level" id="roll" placeholder="Custom roll..." />
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="span3" id="rightCol">
+
 						<div id="log" class="tabbable">
 							<ul class="nav nav-tabs">
 								<li class="active">
@@ -127,22 +101,41 @@
 								</div>
 							</div>
 						</div>
-						<div id="customRoller">
-							<div class="control-group" id="rollerContainer">
-								<div class="controls">
-									<div class="input-append">
-										<input type="text" class="input-block-level" id="roll" placeholder="Custom roll..." />
-									</div>
-								</div>
-							</div>
-						</div>
 					</div>
+
 				</div>
 			</div>
+
 			<div id="popup">
-				<div id="simple">
-					<i id="extraToggle" class="icon-arrow-right"></i>
-					simple stuff
+				<div id="popupLeft">
+					<div id="simple">
+						<div class="input-append input-prepend" style="width: 100%; text-align: center;">
+							<label class="control-label add-on">Attribute:</label>
+
+							<select id="filters" style="width: 150px;">
+							</select>
+
+							<select name="" id="" class="numericOpt" style="width: 75px;">
+								<option value="">></option>
+								<option value="">>=</option>
+								<option value="">=</option>
+								<option value=""><=</option>
+								<option value=""><</option>
+							</select>
+
+							<input type="number" class="numericOpt" style="width: 50px;" placeholder="####" />
+
+							<select class="joinOpt" style="width: 139px; display: none;"></select>
+							
+							<input type="text" id="autocompleteName" class="nameOpt" style="width: 159px; display: none;" placeholder="Enter a name..." />
+							<button class="btn btn-primary numericOpt joinOpt" id="newFilter" type="button">+</button>
+						</div>
+					</div>
+					<div id="median">
+						<div id="medianArrowContainer" title="More options">
+							<i id="extraToggle" class="icon-arrow-right"></i>
+						</div>
+					</div>
 				</div>
 				<div id="extra">
 					extra stuff
--- js/init.js
+++ js/init.js
@@ -1,5 +1,90 @@
 $(function() {
-		
+
+	initializeScrollbars();
+
+	initializeArrowToggler();
+
+	initializePopupToggler();
+
+	initializeFilterSelect();
+
+	$("#filters").find('option[value="Challenge Rating"]').attr('selected','selected');
+
+	$("#hasScript").show();
+
+});
+
+var defaultPopupSize=500;
+var extendPopupSize	=1200;
+			
+var myScroll;
+var resizeTimer;
+var height = 360;
+var heightAdjust = 140;
+
+function initializeFilterSelect() {
+	for(var o in filterData) {
+		$("#filters").append("<option value='"+o+"'>"+o+"</option>");
+	}
+
+	$("#filters").change(function() {
+
+		$(".joinOpt, .numericOpt, .nameOpt").hide();
+
+		//is joinable
+		if(filterData[$(this).val()] instanceof Array) {
+			$(".joinOpt").fadeIn();
+
+		//is numeric
+		} else if(filterData[$(this).val()]){
+			$(".numericOpt").fadeIn();
+
+		//is name
+		} else {
+			$(".nameOpt").fadeIn();
+
+		}
+	});
+}
+
+function initializePopupToggler() {
+	//toggle the sizes between the two popup possibilities
+	$("#medianArrowContainer").click(function() {
+		if($("#extra").is(":visible")) {
+			$("#popup").animate({width: defaultPopupSize});
+			$("#popupLeft").animate({width: defaultPopupSize});
+			$("#extra").animate({width: 0}, function() {
+				$("#extra").css('display', 'none');
+				$("#extraToggle").attr('class', 'icon-arrow-right');	
+				$("#medianArrowContainer").attr('title', 'More options');	
+			});
+		} else {
+			$("#popup").animate({width: extendPopupSize}, function() {
+				$("#popupLeft").animate({width: defaultPopupSize});
+				$("#extra").css('display', 'block');
+				$("#extra").animate({width: extendPopupSize-defaultPopupSize-10});
+				$("#extraToggle").attr('class', 'icon-arrow-left');	
+				$("#medianArrowContainer").attr('title', 'Less options');	
+			});
+		}
+	});
+}
+
+function initializeArrowToggler() {
+	//change the arrow from up to down
+	$("#showFilters").click(function() {
+		$("#popup").slideToggle(400, function() {
+			if($(this).is(":visible")) {
+				$("#filterIcon").attr('class', 'icon-arrow-up');	
+			} else {
+				$("#filterIcon").attr('class', 'icon-arrow-down');
+			}
+		});
+	});
+}
+
+function initializeScrollbars() {
+	//scrollbar for the monster list
 	window.addEventListener('load', function() {
 		setTimeout(function () {
 			myScroll = new iScroll('monsterListCont', {
@@ -11,43 +96,7 @@ $(function() {
 	}, false);
 
 	$(window).resize(timedResizeElements);
-
-	$("#showFilters").click(function() {
-		$("#popup").slideToggle(400, function() {
-			if($(this).is(":visible")) {
-				$("#filterIcon").attr('class', 'icon-arrow-up');	
-			} else {
-				$("#filterIcon").attr('class', 'icon-arrow-down');
-			}
-		});
-	});
-
-	$("#extraToggle").click(function() {
-		if($("#extra").is(":visible")) {
-			$("#popup").animate({width: defaultPopupSize});
-			$("#simple").animate({width: defaultPopupSize});
-			$("#extra").animate({width: 0}, function() {
-				$("#extra").css('display', 'none');
-			});
-		} else {
-			$("#popup").animate({width: extendPopupSize}, function() {
-				$("#simple").animate({width: defaultPopupSize});
-				$("#extra").css('display', 'block');
-				$("#extra").animate({width: extendPopupSize-defaultPopupSize-10});
-			});
-		}
-	});
-
-	$("#hasScript").show();
-});
-
-var defaultPopupSize=300;
-var extendPopupSize	=1000;
-			
-var myScroll;
-var resizeTimer;
-var height = 360;
-var heightAdjust = 140;
+}
 
 //don't instantly automatically refresh everything, that's going to lag
 function timedResizeElements() {
@@ -59,7 +108,7 @@ function resizeElements() {
 	height = $(window).height();
 
 	$("#log").css('height', height-50-heightAdjust+'px');
-	$("#monsterListCont").css('height',height-heightAdjust+'px');
+	$("#monsterListCont").css('height',height-50-heightAdjust+'px');
 	$(".cScrollbarV").css('height',height-heightAdjust+'px');
 
 	myScroll.refresh();
