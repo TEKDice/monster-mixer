fbdfda21061ad155910d233e2942f8d810d02629
Kyle Kemp <imnothere199@gmail.com>
2013-02-12 09:10:52 -0600

    adding per-app navbar

---
+++ nav.php
@@ -0,0 +1,12 @@
+
+<ul class='nav pull-right'>
+	<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+			<i class='icon-cog'></i>
+			<b class="caret"></b>
+		</a>
+		<ul class="dropdown-menu">
+			<li><a>Sessions</a></li>
+		</ul>
+	</li>
+</ul>
