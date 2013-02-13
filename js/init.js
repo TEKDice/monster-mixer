30745bc5eaf3784aa59feefda736b14b57d7ddd5
Kyle Kemp <imnothere199@gmail.com>
2013-02-12 21:50:46 -0600

    working on the session framework

--- js/init.js
+++ js/init.js
@@ -28,4 +28,6 @@ $(function() {
 
 	loadFilters();
 
+	sessionManagement();
+
 });
--- nav.php
+++ nav.php
@@ -10,3 +10,19 @@
 		</ul>
 	</li>
 </ul>
+
+<div id="sessionDialog" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="session" aria-hidden="true">
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+		<h3 id="sessionDialogHeader">Saved Sessions</h3>
+	</div>
+	<div class="modal-body">
+		<form class="form-horizontal">
+		</form>
+	</div>
+	<div class="modal-footer">
+		<span id="sessionDialogError" class="pull-left label label-important"></span>
+		<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+		<button class="btn btn-primary">Sign in</button>
+	</div>
+</div>
