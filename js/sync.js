12867b105bba662183270c564c03f42b998ae35a
Kyle Kemp <imnothere199@gmail.com>
2013-02-14 19:56:41 -0600

    ability to change icon via status

--- js/sync.js
+++ js/sync.js
@@ -183,3 +183,16 @@ function _currentSessionId() {
 function now() {
 	return new Date().getTime();
 }
+
+function changeStatus(status) {
+	switch(status) {
+		case "ok": 				updateTextIcon("icon-ok", "Ready"); return;
+		case "syncing": 		updateTextIcon("icon-refresh", "Syncing..."); return;
+ 		case "noconnection": 	updateTextIcon("icon-warning-sign", "No connection"); return;
+	}
+}
+
+function updateTextIcon(iconClass, text) {
+	$("#noChangeColor").attr('class','').addClass(iconClass);
+	$("#statusText").text(text);
+}
