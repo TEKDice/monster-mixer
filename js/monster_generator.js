b4b55641bbdb6abc2b973fab284cde2d6eb6fe56
Kyle <kyle@tekdice.com>
2013-05-16 02:46:25 -0500

    I bet that fucking function will work now

--- js/monster_generator.js
+++ js/monster_generator.js
@@ -2701,69 +2701,6 @@ function updateTextIcon(iconClass, text) {
 	$("#noChangeColor").attr('class','').addClass(iconClass);
 	$("#statusText").text(text);
 }
-///#source 1 1 /monsters/js/ui.log.js
-
-var logTimer;
-var newLogEntryWidth;
-
-function addToLog(string, selector, uid) {
-
-	//bleh, damned inability to append the same element in two places..
-	var divString = "<div class='logInner'><div class='attackSide'><p class='pull-left "+selector+"' data-uid='"+uid+"'>"+
-		string+"</p></div><div class='pull-right threat-status "+
-		selector+"'></div><div class='clearfix'></div></div>";
-
-	$("#allInfo").append(divString);
-	$$(uid+"_log").append(divString);
-
-	$("#log").find('a').tooltip({html: true});
-
-	clearTimeout(logTimer);
-	logTimer = setTimeout( function() {
-		$("#allInfo").animate( {scrollTop: $("#allInfo")[0].scrollHeight }, 50);
-		$("#curMon").animate( {scrollTop: $("#curMon")[0].scrollHeight }, 50);
-	}, 100);
-
-	$("#log .tab-pane > div").niceScroll({horizrailenabled: false});
-	$("#log .tab-pane > div").css('overflow','hidden');
-
-	$(".attackSide").width(newLogEntryWidth);
-
-}
-
-function changeLogEntrySize() {
-	newLogEntryWidth = $("#allInfo").width()-70;
-}
-
-var logMessages = {
-	skill: function(ent, att, text, num) {
-		return ent + " rolled <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a> on its \""+att+"\" check."
-	},
-
-	hit: function(ent, att, text, num) {
-		return ent + " hits with \""+att+"\" for <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a> damage."
-	},
-
-	initiate: function(ent, att, text, num) {
-		return ent + " attacked with \""+att+"\" rolling <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
-	},
-
-	critAttempt: function(ent, att, text, num) {
-		return ent + " attempted to crit using \""+att+"\" for <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
-	},
-
-	critSecond: function(ent, att, text, num) {
-		return ent + " rolls critical confirm using \""+att+"\" for <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
-	},
-
-	critMiss: function(ent, att, text, num) {
-		return ">> If "+ent+" fails the critical, the damage is <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
-	},
-
-	critSuccess: function(ent, att, text, num) {
-		return ">> If "+ent+" confirms the critical, the damage is <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
-	}
-};
 ///#source 1 1 /monsters/js/ui.filters.js
 
 function loadFilters() {
@@ -3398,3 +3335,66 @@ function _makeSelect(monster, data) {
 	$select.append("<option value='null' selected>No Org.</option>");
 }
 
+///#source 1 1 /monsters/js/ui.log.js
+
+var logTimer;
+var newLogEntryWidth;
+
+function addToLog(string, selector, uid) {
+
+	//bleh, damned inability to append the same element in two places..
+	var divString = "<div class='logInner'><div class='attackSide'><p class='pull-left "+selector+"' data-uid='"+uid+"'>"+
+		string+"</p></div><div class='pull-right threat-status "+
+		selector+"'></div><div class='clearfix'></div></div>";
+
+	$("#allInfo").append(divString);
+	$$(uid+"_log").append(divString);
+
+	$("#log").find('a').tooltip({html: true});
+
+	clearTimeout(logTimer);
+	logTimer = setTimeout( function() {
+		$("#allInfo").animate( {scrollTop: $("#allInfo")[0].scrollHeight }, 50);
+		$("#curMon").animate( {scrollTop: $("#curMon")[0].scrollHeight }, 50);
+	}, 100);
+
+	$("#log .tab-pane > div").niceScroll({horizrailenabled: false});
+	$("#log .tab-pane > div").css('overflow','hidden');
+
+	$(".attackSide").width(newLogEntryWidth);
+
+}
+
+function changeLogEntrySize() {
+	newLogEntryWidth = $("#allInfo").width()-70;
+}
+
+var logMessages = {
+	skill: function(ent, att, text, num) {
+		return ent + " rolled <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a> on its \""+att+"\" check."
+	},
+
+	hit: function(ent, att, text, num) {
+		return ent + " hits with \""+att+"\" for <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a> damage."
+	},
+
+	initiate: function(ent, att, text, num) {
+		return ent + " attacked with \""+att+"\" rolling <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
+	},
+
+	critAttempt: function(ent, att, text, num) {
+		return ent + " attempted to crit using \""+att+"\" for <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
+	},
+
+	critSecond: function(ent, att, text, num) {
+		return ent + " rolls critical confirm using \""+att+"\" for <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
+	},
+
+	critMiss: function(ent, att, text, num) {
+		return ">> If "+ent+" fails the critical, the damage is <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
+	},
+
+	critSuccess: function(ent, att, text, num) {
+		return ">> If "+ent+" confirms the critical, the damage is <a rel='tooltip' href='#' title='"+text+"'>"+num+"</a>."
+	}
+};
