816d7425ff5aab865dcfc63c065a5105212fcc14
Kyle <kyle@tekdice.com>
2013-05-30 23:46:23 -0500

    cleaned up SessionModel code and regioned it off

--- js/monster_generator.js
+++ js/monster_generator.js
@@ -2929,12 +2929,15 @@ var LAST_SESSION_VARIABLE = "lastSessionId";
 var SessionModel = function() {
 	var self = this;
 
+	//#region Dummy/Invalidation
 	self.dummy = ko.observable();
 
 	self.invalidate = function () {
 		self.dummy.notifySubscribers();
 	};
+	//#endregion
 
+	//#region Global Session Data
 	self._currentSessionId = function () {
 		self.invalidate();
 		return self.currentSessionId().toString();
@@ -2961,8 +2964,9 @@ var SessionModel = function() {
 			Data.setVar(SESSIONS_VARIABLE, self.verifySessionData(value));
 		}
 	});
+	//#endregion
 
-
+	//#region Session Filtering
 	self.getSessionById = function (id) {
 		return self.allSessions()[id];
 	};
@@ -2994,29 +2998,24 @@ var SessionModel = function() {
 		return self.syncedSessions().concat(self.nonSyncedSessions());
 	});
 
-	self.isSynced = function(session) {
+	self.verifySessionData = function (data) {
+		var arr = {};
+		$.each(data, function (i, e) {
+			if (self.hasMonsterDataFor(e.startTime)) arr[e.startTime] = e;
+		});
+		return arr;
+	};
+
+	self.isSynced = function (session) {
 		var ret = false;
 		$.each(self.syncedSessions(), function (i, e) {
 			if (session.startTime == e.startTime) ret = true;
 		});
 		return ret;
 	};
+	//#endregion
 
-	self.verifySessionData = function (data) {
-		var arr = {};
-		$.each(data, function (i,e) {
-			if (self.hasMonsterDataFor(e.startTime)) arr[e.startTime] = e;
-		});
-		return arr;
-	};
-
-	self.loadSession = function (id) {
-		if (!loggedIn) return;
-		removeAllMonsters();
-		self.currentSessionId(id);
-		loadMonsters(self.getMonsterDataBySession(id));
-	};
-
+	//#region Saving Functions
 	self.saveSession = function (ask, sessionData) {
 		if (!loggedIn) return;
 
@@ -3044,6 +3043,58 @@ var SessionModel = function() {
 		self.allSessions(sessions);
 	};
 
+	self.saveNewSession = function () {
+		var sessions = self.allSessions();
+
+		var ret = sessions[self._currentSessionId()] = self.newSessionData();
+
+		self.allSessions(sessions);
+		return ret;
+	};
+
+	self.saveMonsters = function (id, data) {
+		Data.setVar("monsters_" + id, data);
+		self.saveSession(false);
+	};
+
+	self.saveCurrentMonsters = function (data) {
+		self.invalidate();
+		self.saveMonsters(self.currentSessionId(), data);
+	};
+
+	self.saveCurrentSessionInfo = function () {
+		var newName = $("#sessionName").val();
+		if (newName == '') return false;
+		var session = sessionManager.currentSession()();
+		session.name = newName;
+		session.lastUpdate = now();
+		self.saveSession(false, session);
+
+		$("#currentSessionDialog").modal('hide');
+		self.invalidate();
+	};
+	//#endregion
+
+	//#region Loading Functions
+	self.loadSession = function (id) {
+		if (!loggedIn) return;
+		removeAllMonsters();
+		self.currentSessionId(id);
+		loadMonsters(self.getMonsterDataBySession(id));
+	};
+
+	self.getPreviousSession = function () {
+		if (!loggedIn) return;
+		if (!Data.hasVar(LAST_SESSION_VARIABLE)) return null;
+		return Data.getVar(LAST_SESSION_VARIABLE);
+	};
+
+	self.getMonsterDataBySession = function (id) {
+		return Data.getVar("monsters_" + id);
+	};
+	//#endregion
+
+	//#region Deletion Functions
 	self.deleteSession = function (uid) {
 		if (!loggedIn) return;
 
@@ -3051,7 +3102,9 @@ var SessionModel = function() {
 		self.allSessions(self.allSessions());
 		self.invalidate();
 	};
+	//#endregion
 
+	//#region Creation Functions
 	self.startSession = function () {
 		if (!loggedIn) return;
 		self.saveSession(false);
@@ -3064,25 +3117,24 @@ var SessionModel = function() {
 		self.saveSession(false);
 		removeAllMonsters();
 	};
+	//#endregion
 
-	self.saveNewSession = function () {
-		var sessions = self.allSessions();
-
-		var ret = sessions[self._currentSessionId()] = self.newSessionData();
-
-		self.allSessions(sessions);
-		return ret;
+	//#region Merging Functions
+	self.mergeCloudSessionsToLocal = function () {
+		var localSessions = self.allSessions();
+		$.each(self.syncedSessions(), function (i, e) {
+			self.saveMonsters(e.startTime, $.parseJSON(e.json));
+			localSessions[e.startTime] = {
+				name: e.name,
+				startTime: parseInt(e.startTime),
+				lastUpdate: parseInt(e.lastUpdate)
+			};
+		});
+		self.allSessions(localSessions);
 	};
+	//#endregion
 
-	self.newSessionData = function () {
-		self.invalidate();
-		return {
-			startTime: self.currentSessionId(),
-			name: "Nameless Campaign",
-			lastUpdate: now()
-		};
-	};
-
+	//#region Check-if Functions
 	self.hasMonsterDataFor = function (id) {
 		return Data.hasVar("monsters_" + id);
 	};
@@ -3095,26 +3147,6 @@ var SessionModel = function() {
 		if (!self.hasMonsterDataFor(lastSessId)) return false;
 		if (self.getMonsterDataBySession(lastSessId).length == 0) return false;
 		return true;
-	};
-
-	self.getPreviousSession = function () {
-		if (!loggedIn) return;
-		if (!Data.hasVar(LAST_SESSION_VARIABLE)) return null;
-		return Data.getVar(LAST_SESSION_VARIABLE);
-	};
-	
-	self.getMonsterDataBySession = function (id) {
-		return Data.getVar("monsters_" + id);
-	};
-
-	self.saveMonsters = function(id, data) {
-		Data.setVar("monsters_" + id, data);
-		self.saveSession(false);
-	};
-
-	self.saveCurrentMonsters = function (data) {
-		self.invalidate();
-		self.saveMonsters(self.currentSessionId(), data);
 	};
 
 	self.sessionManagement = function () {
@@ -3130,42 +3162,33 @@ var SessionModel = function() {
 			self.startSession();
 		}
 	};
+	//#endregion
+
+	//#region Data Creation/Formatting Functions
+	self.newSessionData = function () {
+		self.invalidate();
+		return {
+			startTime: self.currentSessionId(),
+			name: "Nameless Campaign",
+			lastUpdate: now()
+		};
+	};
 
 	self.formatSessionDialogDate = function (date) {
 		return new Date(parseInt(date)).format();
 	};
+	//#endregion
 
-	self.saveCurrentSessionInfo = function () {
-		var newName = $("#sessionName").val();
-		if (newName == '') return false;
-		var session = sessionManager.currentSession()();
-		session.name = newName;
-		session.lastUpdate = now();
-		self.saveSession(false, session);
-
-		$("#currentSessionDialog").modal('hide');
-		self.invalidate();
-	};
-
+	//#region Error Message Functions
 	self.sessionErrorMessage = function (str, isError) {
 		$("#sessionDialogError")
 			.show()
 			.attr('class', 'pull-left label').addClass(isError ? 'label-important' : 'label-success')
 			.text(str);
 	};
+	//#endregion
 
-	self.updateSyncedSessions = function () {
-		$.ajax("sessions.php", {
-			type: "POST",
-			data: {
-				action: "get"
-			}
-		}).done(function (data) {
-			self.syncedSessions($.parseJSON(data));
-		}).fail(function () {
-			self.sessionErrorMessage("Error: Couldn't connect to cloud", true);
-		});
-	};
+	//#region Sessions Menu Button Functions
 	self.syncSessionDeleteButton = function (e) {
 		$("#sessionDialog").modal('hide');
 		bootbox.confirm("Are you sure you want to delete this campaign? Not even a wish can bring this back once it's gone.", function (result) {
@@ -3220,18 +3243,19 @@ var SessionModel = function() {
 		});
 	};
 
-	self.mergeCloudSessionsToLocal = function () {
-		var localSessions = self.allSessions();
-		$.each(self.syncedSessions(), function (i, e) {
-			self.saveMonsters(e.startTime, $.parseJSON(e.json));
-			localSessions[e.startTime] = {
-				name: e.name,
-				startTime: parseInt(e.startTime),
-				lastUpdate: parseInt(e.lastUpdate)
-			};
+	self.updateSyncedSessions = function () {
+		$.ajax("sessions.php", {
+			type: "POST",
+			data: {
+				action: "get"
+			}
+		}).done(function (data) {
+			self.syncedSessions($.parseJSON(data));
+		}).fail(function () {
+			self.sessionErrorMessage("Error: Couldn't connect to cloud", true);
 		});
-		self.allSessions(localSessions);
 	};
+	//#endregion
 };
 
 var sessionManager;
