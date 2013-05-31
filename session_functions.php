105cbddd2dbbf5284a6fd7046654360165e646b9
Kyle <kyle@tekdice.com>
2013-05-31 17:05:00 -0500

    reverting to old php

--- session_functions.php
+++ session_functions.php
@@ -9,9 +9,8 @@ $password = "u53RsT4Bl3!sg0Od";
 
 $sconn = new mysqli($server, $user, $password, $database);
 
-if(isset($_SESSION) && isset($_SESSION["username"])) { 
+if(isset($_SESSION) && isset($_SESSION["username"]))
     $userid = getUserId($_SESSION["username"]);
-}
 
 function getUserId($username) {
     global $sconn;
