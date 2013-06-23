3a7856ac36944f3d49e4158e0fb334e088e5e47e
Kyle <kyle@tekdice.com>
2013-06-23 18:14:35 -0500

    renaming some files

---
+++ js/ui.monsterlist.models.js
@@ -0,0 +1,28 @@
+﻿
+
+var MonsterListModel = function () {
+	var self = this;
+
+	self.monsters = ko.observable({});
+
+	self.getMonster = function(uid) {
+		return self.monsters()[uid];
+	};
+
+	self.addMonster = function (uid, monster) {
+		var monsters = self.monsters();
+		if (!monsters) monsters = {};
+		monsters[uid] = monster;
+		self.monsters(monsters);
+	};
+
+	self.allMonsters = function () {
+		return self.monsters();
+	};
+};
+
+var monsters;
+
+function initializeMonsterModel() {
+	monsters = new MonsterListModel();
+}
